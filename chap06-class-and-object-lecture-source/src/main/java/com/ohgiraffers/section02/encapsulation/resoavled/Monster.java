4ab9b5ac99daa97c824bbd96daea902a6f7852a2
Seo Seungyeop <toa2ek3@naver.com>
2024-01-16 16:26:58 +0900

    class, encapsulation, abstraction, constructor

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/resoavled/Monster.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section02.encapsulation.resoavled;
+
+public class Monster  {
+    private String name;
+    private int hp;
+    public void setInfo1(String info1){
+        this.name = info1;
+    }
+    public void setInfo2(int info2){
+        this.hp = info2;
+    }
+}
