7c30da67593e2346a9955091159e859ef9b9e3b2
Seo Seungyeop <toa2ek3@naver.com>
2024-02-22 11:24:38 +0900

    chap04 mapper-element

---
+++ mybatis/chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/MenuAndCategoryDTO.java
@@ -0,0 +1,74 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+import jdk.jfr.Category;
+
+public class MenuAndCategoryDTO {
+
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+    private CategoryDTO category;
+    private String orderableStatus;
+
+    public MenuAndCategoryDTO() {
+    }
+
+    public MenuAndCategoryDTO(int menuCode, String menuName, int menuPrice, CategoryDTO category, String orderableStatus) {
+        this.menuCode = menuCode;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.category = category;
+        this.orderableStatus = orderableStatus;
+    }
+
+    public int getMenuCode() {
+        return menuCode;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+
+    public CategoryDTO getCategory() {
+        return category;
+    }
+
+    public String getOrderableStatus() {
+        return orderableStatus;
+    }
+
+    public void setMenuCode(int menuCode) {
+        this.menuCode = menuCode;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public void setMenuPrice(int menuPrice) {
+        this.menuPrice = menuPrice;
+    }
+
+    public void setCategory(CategoryDTO category) {
+        this.category = category;
+    }
+
+    public void setOrderableStatus(String orderableStatus) {
+        this.orderableStatus = orderableStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuAndCategoryDTO{" +
+                "menuCode=" + menuCode +
+                ", menuName='" + menuName + '\'' +
+                ", menuPrice=" + menuPrice +
+                ", category=" + category +
+                ", orderableStatus='" + orderableStatus + '\'' +
+                '}';
+    }
+}
