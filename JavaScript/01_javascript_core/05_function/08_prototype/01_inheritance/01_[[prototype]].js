fbca8f72e908c4b8d694fb7f964820aab75fbfa6
Seo Seungyeop <toa2ek3@naver.com>
2024-03-20 12:48:33 +0900

    :memo: [Add] css, javascript

---
+++ JavaScript/01_javascript_core/05_function/08_prototype/01_inheritance/01_[[prototype]].js
@@ -0,0 +1,12 @@
+/* 01. [[prototype]](프로토타입 객체) */
+/* 모든 객체는 프로토타입 객체에 접근할 수 있다. */
+
+const user = {
+    active: true,
+    login: function() {
+        console.log('로그인 되었습니다.');
+    }
+};
+
+
+
