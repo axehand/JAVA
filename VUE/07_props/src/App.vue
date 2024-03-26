c795cf21b34b5628e982a02e2605418ac250a15e
Seo Seungyeop <toa2ek3@naver.com>
2024-03-26 10:45:25 +0900

    :memo: [Add] vue 7번 까지 추가

---
+++ VUE/07_props/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import PropsParent from './components/01_propsBasic/PropsParents.vue'
+  import DrilingParent from './components/02_propsDriling/DrilingParents.vue'
+</script>
+
+<template>
+  <header>
+    <PropsParent/>
+  </header>
+  
+  <main>
+    <DrilingParent/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ VUE/07_props/src/components/02_propsDriling/DrilingChild.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>child: {{ injectCount }}</h2>
+    <GrandChild/>
+
+</template>
+
+<script setup>
+    import {inject} from 'vue';
+    import GrandChild from './DrilingGrandChild.vue';
+
+
+    const injectCount = inject('count');
+
+</script>
